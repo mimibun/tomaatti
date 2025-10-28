259b2c41d57c25e8554d95044bad5d8ff3f8fe11
mimibun <166717259+mimibun@users.noreply.github.com>
2025-10-28 11:15:22 +0100

    feat: build prep

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,10 @@
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		adapter: adapter(),
 		alias: {
 			$scss: "src/scss"
 		},
